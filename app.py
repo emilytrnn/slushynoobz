acc16d229d9024ca8ca7b8831a19b903dc5541f1
Emily Tran <emilytt4@uci.edu>
2023-12-17 03:17:06 -0800

    changed docs

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import sqlite3
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__, template_folder='docs')
 
 def query_database(search_term):
     conn = sqlite3.connect('transcripts.db')
